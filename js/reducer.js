17397efe936e7636c9d5077b47acab43b512299e
Levi Miller <paullevimiller@gmail.com>
2019-12-13 15:51:30 -0800

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,26 @@
 // add code snippets from README
+let state;
+
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  };
+};
+
+function dispatch(action){
+  state = reducer(state, action);
+  render();
+};
+
+function render(){
+  document.getElementById('container').textContent = state.count;
+};
+
+dispatch({type: '@@init'});
+
+document.getElementById('button').addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'});
+});
